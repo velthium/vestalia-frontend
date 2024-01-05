4e54f3228c445331515b8161c2fe7e2244d122b1
Velthium <velthium@gmail.com>
2024-01-05 19:09:32 +0100

    improve header design, add favicon and logo
    - Improve header design
    - Add favicon
    - Add logo

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return(
     <BrowserRouter>
-      <div className="App">
+      <div className="App container">
         <Header />
         <h1>Welcome to Vestalia</h1>
         <Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
+import logo from "../assets/images/VestaliaLogo.webp"
 import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <header>
-      <nav>
-        <ul class="list-unstyled">
-          <li>
-            <Link to="/">Home</Link>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand">
+          <img className="rounded" src={logo} alt="Vestalia Logo" style={{ height: '50px' }} />
+        </Link>
+        <ul class="list-unstyled navbar-nav me-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/profil">Profil</Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/profil">Profil</Link>
           </li>
         </ul>
       </nav>
