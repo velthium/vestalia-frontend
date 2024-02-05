bbc2e05402344590c0b0ede01ed69048b6396c33
Velthium <velthium@gmail.com>
2024-02-05 14:34:19 +0100

    Improve "Connect your Wallet" page design
    - Add Keplr Logo
    - Improve the "Connect your Keplr wallet" area (To be more attrative)

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,7 @@
 import PageTitle from "@/components/Design/PageTitle";
 import { useNavigate } from "react-router-dom";
 import React, { useState } from "react";
+import KeplrLogo from "@/assets/images/Keplr.svg"
 
 function AuthPage() {
     const [pagination, setPagination] = useState({});
@@ -14,7 +15,10 @@ function AuthPage() {
     return (
         <div className="container">
             <PageTitle title="Connect your Wallet" />
-            <button  type="button" className="btn btn-primary w-50 m-auto" onClick={handleClick}>Keplr </button>
+            <button type="button" className="btn bg-sand border shadow-sm w-25 m-auto d-flex" onClick={handleClick}>
+                <img src={KeplrLogo} alt="" />
+                <p className="fw-semibold fs-4 my-auto ms-3">Keplr</p>
+            </button>
     </div>
 );
 }
