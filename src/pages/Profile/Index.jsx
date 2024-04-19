f989d0de7dad41e74eb13d8d4dfb34216fc75bf2
Velthium <velthium@gmail.com>
2024-04-19 23:19:20 +0200

    Remove useless component, optimize profile page
    - Remove useless component (useless because only used one time -> better to regroup it in his Profile/index.jsx parent)
    - Optimisation of the profile page (remove useless fetch -> the data is already loaded in the AuthContext)

--- src/pages/Profile/Index.jsx
+++ src/pages/Profile/Index.jsx
@@ -1,29 +1,125 @@
-import { useQuery } from "@tanstack/react-query";
-import Profile from "@/components/Main/Profile";
+import React, { useContext, useState, useEffect } from "react";
+import SuccessAlert from "@/components/Alert/Success";
+import Keplr from "@/components/Main/Wallet/Keplr";
+import ErrorAlert from "@/components/Alert/Error";
+import { useNavigate } from "react-router-dom";
 import PageTitle from "@/components/Ui/Title";
+import { Profiles } from "@desmoslabs/desmjs";
 import { AuthContext } from "@/context/Auth";
-import React, { useContext } from "react";
-import Error from "@/components/Ui/Error";
 
 function ProfilePage() {
-  const { authData } = useContext(AuthContext);
+  const { authData, setAuthData } = useContext(AuthContext);
+  const [success, setSuccess] = useState(null);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-  const { data: desmosProfile, isLoading, isError } = useQuery({
-    queryKey: ["desmosProfile"],
-    queryFn: async () => {
-      const response = await fetch(`https://api.mainnet.desmos.network/desmos/profiles/v3/profiles/${authData.walletSigner.signer.accountData.address}`)
-        .then(res => (res.json()).then(res => (res.profile)));
-
-      return response;
-    }
+  const [formValues, setFormValues] = useState({
+    username: authData.desmosProfile.nickname || "",
+    dtag: authData.desmosProfile.dtag || "",
+    bio: authData.desmosProfile.bio || ""
   });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <Error message="Error fetching profile data." />;
+  // Save the profile on the blockchain
+  useEffect(() => {
+    setFormValues({
+      username: authData.desmosProfile.nickname || "",
+      dtag: authData.desmosProfile.dtag || "",
+      bio: authData.desmosProfile.bio || ""
+    });
+  }, [authData.desmosProfile.nickname, authData.desmosProfile.dtag, authData.desmosProfile.bio]);
+
+  const handleSaveProfile = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData(e.target);
+      const keplrData = await Keplr();
+
+      const saveProfile = {
+        typeUrl: Profiles.v3.MsgSaveProfileTypeUrl,
+        value: {
+          creator: keplrData.signer.accountData.address,
+          bio: formData.get("bio"),
+          dtag: formData.get("dtag"),
+          nickname: formData.get("username"),
+          coverPicture: "https://ipfs.io/ipfs/<CID>",
+          profilePicture: "https://ipfs.io/ipfs/<CID>"
+        }
+      };
+
+      const result = await keplrData.signAndBroadcast(saveProfile.value.creator, [saveProfile], "auto");
+      setSuccess(result);
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  const handleClearSessionStorage = () => {
+    sessionStorage.clear();
+    setAuthData({});
+    navigate("/");
+  };
+
+  const MyPostsPage = () => {
+    navigate(`/user/${authData.walletSigner.signer.accountData.address}/posts`);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value
+    }));
+  };
+
   return (
       <div>
           <PageTitle title="Modify your profile" />
-          <Profile dtag={desmosProfile.dtag} nickname={desmosProfile.nickname} bio={desmosProfile.bio} wallet={authData.walletSigner.signer.accountData.address}/>
+          <div>
+            <form className="align-left" onSubmit={handleSaveProfile}>
+              <div className="mb-3">
+                <label className="form-label" htmlFor="username">Username:</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="username"
+                  id="username"
+                  value={formValues.username}
+                  onChange={handleInputChange}
+                  placeholder="Enter a username"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label" htmlFor="dtag">Dtag:</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="dtag"
+                  id="dtag"
+                  value={formValues.dtag}
+                  onChange={handleInputChange}
+                  placeholder="Enter a dtag"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label" htmlFor="bio">Bio:</label>
+                <textarea
+                  className="form-control"
+                  type="text"
+                  name="bio"
+                  id="bio"
+                  value={formValues.bio}
+                  onChange={handleInputChange}
+                  placeholder="Enter a bio"
+                />
+              </div>
+              <button className="btn btn-info text-light" type="submit">Submit</button>
+            </form>
+            {success && <SuccessAlert success={success} />}
+            {error && <ErrorAlert error={error} />}
+            <button className="btn btn-secondary text-light m-2" onClick={MyPostsPage}>Your posts</button>
+            <button className="btn btn-danger text-light m-2" onClick={handleClearSessionStorage}>Logout</button>
+          </div>
       </div>
   );
 }
